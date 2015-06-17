633636b6f048d34d271190411935c1f7d0c28041
jirikadlec2 <jirikadlec2@gmail.com>
2015-06-17 11:38:05 -0600

    More error checking in GetSites

--- R/GetSites.R
+++ R/GetSites.R
@@ -26,15 +26,25 @@
 
 GetSites <- function(server, west=NULL, south=NULL, east=NULL, north=NULL) {
 
+  # trim any leading and trailing whitespaces in server
+  server <- gsub("^\\s+|\\s+$", "", server)
+
   # if server ends with ?WSDL or ?wsdl, we assume that service is SOAP
   # otherwise, assume that service is REST
   SOAP <- TRUE
+
+  # if server ends with .asmx, we also assume that the service is SOAP and we add ?WSDL
+  m1 <- regexpr("asmx$", server)
+  if (m1 > 1) {
+    server <- paste(server, "WSDL", sep="?")
+  }
+
   m <- regexpr("?WSDL|wsdl", server)
   if (m > 1) {
     url <- substr(server, 0, m - 2)
     SOAP <- TRUE
   } else {
-    url <- paste(server, "?site=&authToken=", sep="")
+    # in other cases we leave the URL as it is
     SOAP <- FALSE
   }
 
@@ -69,87 +79,106 @@ GetSites <- function(server, west=NULL, south=NULL, east=NULL, north=NULL) {
 
     print(paste("download time:", download.time["elapsed"], "seconds, status:", status.code))
 
-    print("reading sites WaterML data...")
-    doc <- tryCatch({
-      content(response)
-    }, warning = function(w) {
-      print("Error reading WaterML: Bad XML format.")
-      return(NULL)
-    }, error = function(e) {
-      print("Error reading WaterML: Bad XML format.")
+    #in case of server error, print the error and exit
+    if (status.code == "server error") {
+      print(http_status(response)$message)
       return(NULL)
     }
-    )
-    if (is.null(doc)) {
-      return(NULL)
-    }
-
-    # specify the namespace information
-    ns <- WaterOneFlowNamespace(version)
-
-    # extract the data columns with XPath
-    SiteName = xpathSApply(doc, "//sr:siteName", xmlValue, namespaces=ns)
-    SiteCode = xpathSApply(doc, "//sr:siteCode", xmlValue, namespaces=ns)
-    Network = xpathSApply(doc, "//sr:siteCode", xmlGetAttr, name="network", namespaces=ns)
-    Latitude = xpathSApply(doc, "//sr:latitude", xmlValue, namespaces=ns)
-    Longitude = xpathSApply(doc, "//sr:longitude", xmlValue, namespaces=ns)
-
-    SiteID <- xpathSApply(doc, "//sr:siteCode", xmlGetAttr, name="siteID", namespaces=ns)
-    SiteID <- unlist(SiteID)
-
-    numSiteIDs <- length(SiteID)
-    numSites <- length(SiteCode)
-    if (numSiteIDs != numSites) {
-      SiteID <- SiteCode
-    }
-
-    Elevation <- xpathSApply(doc, "//sr:elevation_m", xmlValue, namespaces=ns)
-    numElevations <- length(Elevation)
-    if (numElevations != numSites) {
-      Elevation <- NA
-    }
-
-    # State, County, Comments: different tags for WaterML 1.0 and 1.1
-    if (version=="1.1"){
-      State = xpathSApply(doc, "//sr:siteProperty[@name='State']", xmlValue, namespaces=ns)
-      County = xpathSApply(doc, "//sr:siteProperty[@name='County']", xmlValue, namespaces=ns)
-      Comments = xpathSApply(doc, "//sr:siteProperty[@name='Site Comments']", xmlValue, namespaces=ns)
-    } else {
-      State = xpathSApply(doc, "//sr:note[@title='State']", xmlValue, namespaces=ns)
-      County = xpathSApply(doc, "//sr:note[@title='County']", xmlValue, namespaces=ns)
-      Comments = NA
-    }
-    # Check for empty values of state, county, comments
-    numStates <- length(State)
-    if (numStates != numSites) {
-      State <- NA
-    }
-    numCounties <- length(County)
-    if (numCounties != numSites) {
-      County <- NA
-    }
-    numComments <- length(Comments)
-    if (numComments != numSites) {
-      Comments <- NA
-    }
-
-    df <- data.frame(
-      SiteID = SiteID,
-      SiteName = SiteName,
-      SiteCode = SiteCode,
-      FullSiteCode = paste(Network, SiteCode, sep=":"),
-      Latitude = as.numeric(Latitude),
-      Longitude = as.numeric(Longitude),
-      Elevation = as.numeric(Elevation),
-      State = State,
-      County = County,
-      Comments = Comments,
-      stringsAsFactors = FALSE)
-
-    return(df)
-
   } else {
-    #if the service is REST
+    # If the service is REST:
+    print(paste("downloading sites from:", server, "..."))
+
+    download.time <- system.time(
+      tryCatch({
+        response <- GET(server)
+      },error=function(e){
+        print(conditionMessage(e))
+        return(NULL)
+      }))
+
+    status.code <- http_status(response)$category
+    print(paste("download time:", download.time["elapsed"], "seconds, status:", status.code))
+  }
+  ######################################################
+  # Parsing the WaterML XML Data                       #
+  ######################################################
+
+  print("reading sites WaterML data...")
+  doc <- tryCatch({
+    content(response)
+  }, warning = function(w) {
+    print("Error reading WaterML: Bad XML format.")
+    return(NULL)
+  }, error = function(e) {
+    print("Error reading WaterML: Bad XML format.")
     return(NULL)
   }
+  )
+  if (is.null(doc)) {
+    return(NULL)
+  }
+
+  # specify the namespace information
+  ns <- WaterOneFlowNamespace(version)
+
+  # extract the data columns with XPath
+  SiteName = xpathSApply(doc, "//sr:siteName", xmlValue, namespaces=ns)
+  SiteCode = xpathSApply(doc, "//sr:siteCode", xmlValue, namespaces=ns)
+  Network = xpathSApply(doc, "//sr:siteCode", xmlGetAttr, name="network", namespaces=ns)
+  Latitude = xpathSApply(doc, "//sr:latitude", xmlValue, namespaces=ns)
+  Longitude = xpathSApply(doc, "//sr:longitude", xmlValue, namespaces=ns)
+
+  SiteID <- xpathSApply(doc, "//sr:siteCode", xmlGetAttr, name="siteID", namespaces=ns)
+  SiteID <- unlist(SiteID)
+
+  numSiteIDs <- length(SiteID)
+  numSites <- length(SiteCode)
+  if (numSiteIDs != numSites) {
+    SiteID <- SiteCode
+  }
+
+  Elevation <- xpathSApply(doc, "//sr:elevation_m", xmlValue, namespaces=ns)
+  numElevations <- length(Elevation)
+  if (numElevations != numSites) {
+    Elevation <- NA
+  }
+
+  # State, County, Comments: different tags for WaterML 1.0 and 1.1
+  if (version=="1.1"){
+    State = xpathSApply(doc, "//sr:siteProperty[@name='State']", xmlValue, namespaces=ns)
+    County = xpathSApply(doc, "//sr:siteProperty[@name='County']", xmlValue, namespaces=ns)
+    Comments = xpathSApply(doc, "//sr:siteProperty[@name='Site Comments']", xmlValue, namespaces=ns)
+  } else {
+    State = xpathSApply(doc, "//sr:note[@title='State']", xmlValue, namespaces=ns)
+    County = xpathSApply(doc, "//sr:note[@title='County']", xmlValue, namespaces=ns)
+    Comments = NA
+  }
+  # Check for empty values of state, county, comments
+  numStates <- length(State)
+  if (numStates != numSites) {
+    State <- NA
+  }
+  numCounties <- length(County)
+  if (numCounties != numSites) {
+    County <- NA
+  }
+  numComments <- length(Comments)
+  if (numComments != numSites) {
+    Comments <- NA
+  }
+
+  df <- data.frame(
+    SiteID = SiteID,
+    SiteName = SiteName,
+    SiteCode = SiteCode,
+    FullSiteCode = paste(Network, SiteCode, sep=":"),
+    Latitude = as.numeric(Latitude),
+    Longitude = as.numeric(Longitude),
+    Elevation = as.numeric(Elevation),
+    State = State,
+    County = County,
+    Comments = Comments,
+    stringsAsFactors = FALSE)
+
+  return(df)
 }
