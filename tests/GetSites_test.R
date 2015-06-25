2e8ff05dbec4e5b28fc8d504be6348a37fb6278c
jirikadlec2 <jirikadlec2@gmail.com>
2015-06-24 22:55:17 -0600

    More changes to GetSites testing

--- tests/GetSites_test.R
+++ tests/GetSites_test.R
@@ -4,9 +4,10 @@ services <- GetServices()
 
 test_result <- data.frame(server=character(0), sites_download_time=numeric(0), sites_download_status=character(0),
                           sites_parse_time=numeric(0), sites_parse_status=character(0),
-                          num_sites=numeric(0), random_site_code=character(0))
+                          num_sites=numeric(0), random_site_code=character(0),
+                          stringsAsFactors=FALSE)
 
-for (i in 1:nrow(services)) {
+for (i in 2:nrow(services)) {
   server <- services$url[i]
   sites <- GetSites(server)
   sites_download_time <- attr(sites, "download.time")
@@ -18,7 +19,10 @@ for (i in 1:nrow(services)) {
   if (num_sites > 0) {
     random_site_code <- sample(sites$FullSiteCode, size=1)
   }
-  new_row <- c(server, sites_download_time, sites_download_status, sites_parse_time,
-               sites_parse_status, num_sites, random_site_code)
-  df <- rbind(df, new_row)
+  new_row <- c(server, as.numeric(sites_download_time),
+                      sites_download_status,
+                      as.numeric(sites_parse_time),
+                      sites_parse_status,
+                      num_sites, random_site_code)
+  test_result[nrow(test_result)+1,] <- new_row
 }
