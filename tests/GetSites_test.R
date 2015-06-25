2a0c5d4343c089acc75d3a8d8ffe32308f794af1
jirikadlec2 <jirikadlec2@gmail.com>
2015-06-24 21:25:31 -0600

    Added a test for running GetSites for all registered HydroServers

--- tests/GetSites_test.R
+++ tests/GetSites_test.R
@@ -1,4 +1,23 @@
 library(WaterML)
 
-server <- "http://ariel.lnec.pt/hs/main/services/cuahsi_1_1.asmx?WSDL"
-sites <- GetSites(server)
+services <- GetServices()
+test_result <- data.frame()
+names(test_result) <- c("server", "sites_download_time", "sites_download_status", "sites_parse_time",
+                        "sites_parse_status", "num_sites", "random_site_code")
+
+for (i in 1:nrow(services)) {
+  server <- services$url[i]
+  sites <- GetSites(server)
+  sites_download_time <- attr(sites, "download.time")
+  sites_download_status <- attr(sites, "download.status")
+  sites_parse_time <- attr(sites, "parse.time")
+  sites_parse_status <- attr(sites, "parse.status")
+  random_site_code <- NA
+  num_sites <- nrow(sites)
+  if (num_sites > 0) {
+    random_site_code <- sample(sites$FullSiteCode, size=1)
+  }
+  new_row <- c(server, sites_download_time, sites_download_status, sites_parse_time,
+               sites_parse_status, num_sites, random_site_code)
+  df <- rbind(df, new_row)
+}
