0ccb1e1cbcf7f71eb21c7688823b55be9f3af57e
jirikadlec2 <jirikadlec2@gmail.com>
2015-09-02 19:11:50 -0600

    Fixed example for MakeSOAPEnvelope (little bear river web service is not working)

--- R/MakeSOAPEnvelope.R
+++ R/MakeSOAPEnvelope.R
@@ -24,7 +24,7 @@
 #' library(httr)
 #' myEnvelope <- MakeSOAPEnvelope("http://www.cuahsi.org/his/1.1/ws/", "GetSitesObject")
 #' SOAPAction <- "http://www.cuahsi.org/his/1.1/ws/GetSitesObject"
-#' url <- "http://icewater.usu.edu/littlebearriver/cuahsi_1_1.asmx"
+#' url <- "http://hydrodata.info/chmi-d/cuahsi_1_1.asmx"
 #' response <- POST(url, body = myEnvelope,
 #'                  add_headers("Content-Type" = "text/xml", "SOAPAction" = SOAPAction),
 #'                  verbose())
